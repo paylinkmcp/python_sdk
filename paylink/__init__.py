97d647d6e05901efb71638dd7ced6e7d54435b75
jameskanyiri <jmskanyiri@gmail.com>
2025-11-15 23:47:06 +0300

    Bump version to 0.2.2

--- paylink/__init__.py
+++ paylink/__init__.py
@@ -8,7 +8,7 @@ from .client import PayLink
 from .mcp_monitization import McpMonitizationAdapter, PaymentError, require_payment
 from .mpesa_tools import MpesaTools
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __author__ = "PayLink"
 __email__ = "paylinkmcp@gmail.com"
 __all__ = ["PayLink", "MpesaTools", "McpMonitizationAdapter", "PaymentError", "require_payment"]
